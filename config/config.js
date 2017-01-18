3b9221d26097fce28687f877b5c620fbdfb6f557
edcompton <eahcompton@gmail.com>
2017-01-18 10:08:18 +0000

    adding process env files

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/project2',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/project2',
   url: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=S6Ne496thElaCfSl25nc9B3NkTEAk0o7&radius=10000&marketId=202&size=300&classificationName=Music'
 };
